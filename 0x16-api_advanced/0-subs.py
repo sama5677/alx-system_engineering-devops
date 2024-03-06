404f7d57413ea14a5ee589e9587b6ec3ea662ce8
sama5677 <samaamohamed687@gmail.com>
2024-03-06 08:15:29 -0800

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
-
+""" Number of subs"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    """ Returns the number of susbs of reddit """
+    if subreddit is None:
+        return (0)
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"user-agent": 'API Project by andreshugueth'}
+    r = requests.get(url, headers=headers).json()
+    subscribers = r.get("data", {}).get("subscribers", 0)
+    return subscribers
